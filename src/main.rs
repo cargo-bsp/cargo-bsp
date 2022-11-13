ce7a2440aa20e369613a414d113c45f333907b26
Toomimi <tmszglab@gmail.com>
2022-11-13 01:41:26 +0100

    Add basic BSP structures implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod bsp_types;
+
+fn example_client_initialize_query()-> bsp_types::InitializeBuildParams<String> {
+    bsp_types::InitializeBuildParams {
+        display_name: "rust-bsp-client".to_string(),
+        version: "0.1.0".to_string(),
+        bsp_version: "2.0.0-M5".to_string(),
+        root_uri: "file:///home/krzysztof/Projects/rust-bsp-client".to_string(),
+        capabilities: bsp_types::BuildClientCapabilities {
+            language_ids: vec!["rust".to_string()],
+        },
+        data: None,
+    }
+}
+
+fn example_client_initialize_query_json() -> String {
+    serde_json::to_string(&example_client_initialize_query()).unwrap()
+}
+
 fn main() {
+    // Note that serde parses Option(None) as null, does not skip it.
+    println!("{}", example_client_initialize_query_json());
     println!("Hello, world!");
 }
