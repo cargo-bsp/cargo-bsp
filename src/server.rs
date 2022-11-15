156b9575b2cc9509dccfdeaf899687ea36cc0578
juliapodrazka <juliapodrazka@poczta.onet.pl>
2022-11-15 19:25:18 +0100

    Add preparing result message for client

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,29 @@
 // You can run this with `cargo run --bin server
 
-// use std::borrow::Borrow;
 use std::io;
 use std::io::prelude::*;
 use std::io::stderr;
-
-// use beef::Cow;
-// use jsonrpsee_types::Id::Null;
-use jsonrpsee_types::Request;
+use jsonrpsee_types::{Id, Request, RequestSer};
 use serde_json::Result;
+use serde_json::value::RawValue;
 
-use crate::bsp_types::InitializeBuildParams;
+use crate::bsp_types::{BuildClientCapabilities, InitializeBuildParams};
 
-// use std::ops::Deref;
-
-// use serde_json::value::RawValue;
 pub fn run_server() {
     stderr().write_all("Server has started\n".as_bytes()).unwrap();
     println!("Hello, it's me - server :)");
+
+
+    let temp: InitializeBuildParams<()> = InitializeBuildParams {
+        display_name: "test1".to_string(),
+        version: "test2".to_string(),
+        bsp_version: "test3".to_string(),
+        root_uri: "test4".to_string(),
+        capabilities: BuildClientCapabilities { language_ids: vec!["test5".to_string()] },
+        data: None,
+    };
+    println!("{}", prepare_result(&temp));
+
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line_string = line.unwrap();
@@ -40,22 +46,20 @@ pub fn run_server() {
     }
 }
 
-
 fn get_request(request_string: &str) -> Result<InitializeBuildParams> {
     let request: Request = serde_json::from_str(request_string)?;
     serde_json::from_str(request.params.map_or("", |x| x.get()))
 }
 
-// {"jsonrpc": "2.0", "method": "subtract", "params": { "displayName": "test1", "version": "test2", "bspVersion": "test3", "rootUri": "test4", "capabilities": { "languageIds": ["test5"] }}, "id": 3}
+fn prepare_result(struct_params: &InitializeBuildParams) -> String {
+    let string_params = serde_json::to_string(struct_params).unwrap();
+    let params = Some(RawValue::from_string(string_params).unwrap());
 
-// let temp: InitializeBuildParams<()> = InitializeBuildParams {
-//     display_name: "test1".to_string(),
-//     version: "test2".to_string(),
-//     bsp_version: "test3".to_string(),
-//     root_uri: "test4".to_string(),
-//     capabilities: BuildClientCapabilities { language_ids: vec!["test5".to_string()] },
-//     data: None
-// };
-// let temp_string = serde_json::to_string(&temp).unwrap();
-// let temp3 = RawValue::from_string(temp_string).unwrap().borrow();
-// let temp2 = Request::new(Cow::borrowed("test6"), Some(temp3), Null);
+    let method = "test6";
+    let id = Id::Number(0183);
+    let result = RequestSer::borrowed(&id, &method, params.as_deref());
+
+    serde_json::to_string(&result).unwrap()
+}
+
+// {"jsonrpc": "2.0", "method": "subtract", "params": { "displayName": "test1", "version": "test2", "bspVersion": "test3", "rootUri": "test4", "capabilities": { "languageIds": ["test5"] }}, "id": 3}
