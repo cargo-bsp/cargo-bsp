0c9553c016cb34b26c7d98566667cab6c3a914a5
Julia Podrażka <73885839+julia-podrazka@users.noreply.github.com>
2023-04-26 23:27:33 +0200

    Add cargo information handling and request actor tests (#31)
    Co-authored-by: Patryk Bundyra <bundyradev@gmail.com>
    Co-authored-by: KatKlo <kasiakloc00@gmail.com>

--- src/bsp_types/basic_bsp_structures.rs
+++ src/bsp_types/basic_bsp_structures.rs
@@ -80,6 +80,12 @@ pub struct BuildTargetIdentifier {
     pub uri: Uri,
 }
 
+impl From<&str> for BuildTargetIdentifier {
+    fn from(uri: &str) -> Self {
+        BuildTargetIdentifier { uri: uri.into() }
+    }
+}
+
 #[derive(Debug, PartialEq, Serialize, Deserialize, Default, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub enum BuildTargetTag {
