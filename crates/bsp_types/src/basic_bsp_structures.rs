e8fffeff531a37cce5e6da001c79a2704f2614fd
KatKlo <katarzyna.kloc@jetbrains.com>
2023-09-01 13:08:08 +0200

    Fix edition mapping bug (#87)

--- crates/bsp_types/src/basic_bsp_structures.rs
+++ crates/bsp_types/src/basic_bsp_structures.rs
@@ -78,9 +78,6 @@ impl Edition {
     pub const E2015: Edition = Edition::new("2015");
     pub const E2018: Edition = Edition::new("2018");
     pub const E2021: Edition = Edition::new("2021");
-    pub const _E2024: Edition = Edition::new("2024");
-    pub const _E2027: Edition = Edition::new("2027");
-    pub const _E2030: Edition = Edition::new("2030");
 
     pub const fn new(tag: &'static str) -> Self {
         Edition(std::borrow::Cow::Borrowed(tag))
